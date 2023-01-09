85bc05d60f97bd0be7e38299a32100615321d2ed
林政浩 <1392872316@qq.com>
2023-01-09 23:44:21 +0800

    0109

--- lethead2.c
+++ lethead2.c
@@ -5,14 +5,14 @@
 #define ADDRESS "101 Megabuck Plaza"
 #define PLACE "Megapolis, CA 94904"
 #define WIDTH 40
-#define SPACE " "
+#define SPACE ' '
 
 void show_n_char(char ch, int num);
 
 int main(void) {
 	int spaces;
 
-	show_n_char("*", WIDTH);		//用符号常量作为参数
+	show_n_char('*', WIDTH);		//用符号常量作为参数
 	putchar('\n');
 	show_n_char(SPACE, 12);		//用符号常量作为参数
 	printf("%s\n", NAME);
